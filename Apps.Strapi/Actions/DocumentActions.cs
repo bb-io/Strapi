c94fd8c80f26d23c82ccacd6d8d4a6eddf25114b
Mihaly Hobor <mihaly.hobor@blackbird.io>
2025-04-04 12:49:42 +0200

    Refactor DocumentActions to integrate Blackbird SDK and update method signatures for action parameters.

--- Apps.Strapi/Actions/DocumentActions.cs
+++ Apps.Strapi/Actions/DocumentActions.cs
@@ -1,6 +1,7 @@
 using Apps.Strapi.Api;
 using Apps.Strapi.Models.Requests;
 using Apps.Strapi.Models.Responses;
+using Blackbird.Applications.Sdk.Common;
 using Blackbird.Applications.Sdk.Common.Actions;
 using Blackbird.Applications.Sdk.Common.Exceptions;
 using Blackbird.Applications.Sdk.Common.Invocation;
@@ -14,7 +15,7 @@ namespace Apps.Strapi.Actions;
 public class DocumentActions(InvocationContext invocationContext) : Invocable(invocationContext)
 {
     [Action("Get Documents", Description = "Gets a list of document.")] //TODO: fill description
-    public async Task<DocumentsResponse> GetDocuments(GetDocumentRequest request)
+    public async Task<DocumentsResponse> GetDocuments([ActionParameter]GetDocumentRequest request)
     {
 
         string query = string.Empty;
@@ -36,7 +37,7 @@ public class DocumentActions(InvocationContext invocationContext) : Invocable(in
     }
 
     [Action("Get Document", Description = "Gets a document.")] //TODO: fill description
-    public async Task<DocumentsResponse> GetDocument(GetDocumentRequest request)
+    public async Task<DocumentsResponse> GetDocument([ActionParameter] GetDocumentRequest request)
     {
         var result = await Client.GetAsync<DocumentsResponse>(new RestRequest($"/:{request.Id}", Method.Get));
 
@@ -47,19 +48,19 @@ public class DocumentActions(InvocationContext invocationContext) : Invocable(in
         throw new Exception();
     }
     [Action("Create Document", Description = "Creates a new document")]//TODO: fill description
-    public async Task<DocumentResponse> CreateDocument(CreateDocumentRequest request)
+    public async Task<DocumentResponse> CreateDocument([ActionParameter] CreateDocumentRequest request)
     {
         throw new NotImplementedException();
     }
 
     [Action("Update Document", Description = "Updates an existing document")]
-    public async Task<DocumentResponse> UpdateDocument(UpdateDocumentRequest request)
+    public async Task<DocumentResponse> UpdateDocument([ActionParameter] UpdateDocumentRequest request)
     {
         throw new NotImplementedException();
     }
 
     [Action("Delete Document", Description = "Deletes a document")]
-    public async Task DeleteDocument(DeleteDocumentRequest request)
+    public async Task DeleteDocument([ActionParameter] DeleteDocumentRequest request)
     {
         throw new NotImplementedException();
     }
