f88be80ce5f2e3c8a0f5997f7e779529ba139d1d
Mihaly Hobor <mihaly.hobor@blackbird.io>
2025-04-04 12:41:33 +0200

    Add properties and using directive in document request classes.

--- Apps.Strapi/Models/Requests/BaseDocumentRequest.cs
+++ Apps.Strapi/Models/Requests/BaseDocumentRequest.cs
@@ -1,7 +1,10 @@
-﻿namespace Models.Requests
+﻿using Blackbird.Applications.Sdk.Common;
+
+namespace Models.Requests
 {
     public class BaseDocumentRequest
     {
+        [Display("Api Id", Description = "Description")]
         public string ApiId { get; set; }
     }
 }
--- Apps.Strapi/Models/Requests/GetDocumentRequest.cs
+++ Apps.Strapi/Models/Requests/GetDocumentRequest.cs
@@ -1,7 +1,10 @@
+using Blackbird.Applications.Sdk.Common;
+
 namespace Models.Requests
 {
     public class GetDocumentRequest : BaseDocumentRequest
     {
+        [Display("Id", Description ="Description")]
         public int Id { get; set; }
     }
 }
