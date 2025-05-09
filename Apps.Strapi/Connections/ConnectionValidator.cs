e5e487f2a846dec3c922414184412cf158e92bee
vitaliy-bezugly <157373241+vitalii-bezuhlyi@users.noreply.github.com>
2025-05-09 17:58:08 +0300

    refactor(ConnectionValidator): 🔧 improve connection validation logic; replace `ExecuteWithErrorHandling` with `ExecuteAsync`; enhance error handling for 404 status

--- Apps.Strapi/Connections/ConnectionValidator.cs
+++ Apps.Strapi/Connections/ConnectionValidator.cs
@@ -1,6 +1,8 @@
 using Apps.Strapi.Api;
+using Apps.Strapi.Models.Dtos;
 using Blackbird.Applications.Sdk.Common.Authentication;
 using Blackbird.Applications.Sdk.Common.Connections;
+using Newtonsoft.Json;
 using RestSharp;
 
 namespace Apps.Strapi.Connections;
@@ -12,11 +14,23 @@ public class ConnectionValidator : IConnectionValidator
         try
         {
             var client = new StrapiClient(authenticationCredentialsProviders);
-            var result = await client.ExecuteWithErrorHandling(new RestRequest());
+            var result = await client.ExecuteAsync(new RestRequest("/api"));
+            
+            if (result.StatusCode == System.Net.HttpStatusCode.NotFound)
+            {
+                var errorDto = JsonConvert.DeserializeObject<ErrorDto>(result.Content!)!;
+                var isValid = errorDto.Error.Status == 404 && errorDto.Error.Name == "NotFoundError";
+                return new()
+                {
+                    IsValid = isValid,
+                    Message = isValid ? "It's not possible to check if the api token is valid, but at least the URL is correct" : result.Content
+                };
+            }
 
             return new()
             {
-                IsValid = true
+                IsValid = false,
+                Message = result.Content
             };
         }
         catch (Exception ex)
