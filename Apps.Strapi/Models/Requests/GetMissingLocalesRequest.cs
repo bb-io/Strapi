7a28161dfee646bd0701c46fa726bd0bc42d722a
vitaliy-bezugly <157373241+vitalii-bezuhlyi@users.noreply.github.com>
2025-08-11 13:15:25 +0300

    fix(GetMissingLocalesRequest): 🐛 update display name for `ContentTypeId` to remove "(singular)" for clarity

--- Apps.Strapi/Models/Requests/GetMissingLocalesRequest.cs
+++ Apps.Strapi/Models/Requests/GetMissingLocalesRequest.cs
@@ -6,7 +6,7 @@ namespace Apps.Strapi.Models.Requests;
 
 public class GetMissingLocalesRequest : VersionOptionalRequest
 {
-    [Display("Content type ID (singular)", Description = "The content type of the document. If you are using strapi v4 this should be the singular form of the content type, e.g., 'article' for 'articles'.")]
+    [Display("Content type ID", Description = "The content type of the document. If you are using strapi v4 this should be the singular form of the content type, e.g., 'article' for 'articles'.")]
     public string ContentTypeId { get; set; } = string.Empty;
 
     [Display("Content ID", Description = "The ID of the content to be downloaded."), DataSource(typeof(ContentDataHandler))]
