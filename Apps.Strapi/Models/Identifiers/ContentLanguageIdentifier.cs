68a0f9f643e4f9458e6d5f1802269f806107b997
vitaliy-bezugly <157373241+vitalii-bezuhlyi@users.noreply.github.com>
2025-09-18 12:59:38 +0300

    fix: make language as optional

--- Apps.Strapi/Models/Identifiers/ContentLanguageIdentifier.cs
+++ Apps.Strapi/Models/Identifiers/ContentLanguageIdentifier.cs
@@ -9,5 +9,5 @@ namespace Apps.Strapi.Models.Identifiers;
 public class ContentLanguageIdentifier : ContentIdentifier
 {
     [Display("Language", Description = "The language of the content"), DataSource(typeof(LanguageDataHandler))]
-    public string Language { get; set; } = string.Empty;
+    public string? Language { get; set; } = string.Empty;
 }
