895cd8cccb66c15efb02d1998ae3ad68f641a69d
vitaliy-bezugly <157373241+vitalii-bezuhlyi@users.noreply.github.com>
2025-10-29 14:35:39 +0200

    Fixed issue with content type kebab-case

---
+++ Apps.Strapi/Utils/ContentTypeUtils.cs
@@ -0,0 +1,16 @@
+﻿namespace Apps.Strapi.Utils;
+
+public static class ContentTypeUtils
+{
+    public static string ConvertToGraphQlContentType(string contentType)
+    {
+        var parts = contentType.Split('-');
+        var graphQlContentType = parts[0].ToLower();
+        for (int i = 1; i < parts.Length; i++)
+        {
+            graphQlContentType += char.ToUpper(parts[i][0]) + parts[i].Substring(1).ToLower();
+        }
+        
+        return graphQlContentType.EndsWith('s') ? graphQlContentType[..^1] : graphQlContentType;
+    }
+}
