093484ffeaffda84f32542cd12e560beeb19c316
Anton Fesenko <tohafesenkolovep@gmail.com>
2026-03-25 10:30:19 +0200

    add IMultiDownloadableContentOutput to polling response, rename response class to PollingSearchContentResponse

--- Apps.Strapi/Models/Responses/DocumentResponse.cs
+++ Apps.Strapi/Models/Responses/DocumentResponse.cs
@@ -1,29 +1,12 @@
 using Blackbird.Applications.SDK.Blueprints.Interfaces.CMS;
 using Blackbird.Applications.Sdk.Common;
 using Newtonsoft.Json;
+using Apps.Strapi.Models.Responses;
 
 namespace Models.Responses;
 
-public class DocumentResponse : IContentOutput
+public class DocumentResponse : BaseDocumentResponse, IContentOutput
 {
     [Display("Content ID"), JsonProperty("documentId")]
-    public string Id { get; set; } = string.Empty;
-
-    [Display("Content type ID")]
-    public string? ContentTypeId { get; set; }
-
-    [Display("Title")]
-    public string? Title { get; set; }
-
-    [Display("Created at")]
-    public DateTime CreatedAt { get; set; }
-
-    [Display("Updated at")]
-    public DateTime UpdatedAt { get; set; }
-
-    [Display("Published at")]
-    public DateTime? PublishedAt { get; set; }
-
-    [Display("Language")]
-    public string? Locale { get; set; }
+    public string Id { get; set; } = string.Empty;    
 }
